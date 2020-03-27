25880f2369aae7717fee28ba5fb42f45eef6a738
Victor Plushko <vicplushko33@gmail.com>
2020-03-27 19:14:14 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -24,8 +24,9 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  
-end
+  array = ["wow", "Luke", "Leia", "Chewie"]
+  array.shift()
+  end
 
 def retrieve_element_from_index(array, index_number)
   
