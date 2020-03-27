587649ee01c9c2a41e13f24fc95c006939a88701
Victor Plushko <vicplushko33@gmail.com>
2020-03-27 18:34:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("element")
+  star_wars_characters.push "C-3PO"
 end
 
 def add_element_to_start_of_array(array, element)
